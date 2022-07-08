438363ca6699ba047348126f57afcae745d851d1
cintiaasilva <cintiazl2015@gmail.com>
2022-07-08 17:11:55 -0300

    Gerenciando imagens usando Picasso

--- Soccer News/app/src/main/java/me/dio/soccernews/ui/adapter/NewsAdapter.java
+++ Soccer News/app/src/main/java/me/dio/soccernews/ui/adapter/NewsAdapter.java
@@ -6,6 +6,8 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.squareup.picasso.Picasso;
+
 import java.util.List;
 
 import me.dio.soccernews.databinding.NewsItemBinding;
@@ -32,6 +34,8 @@ public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.ViewHolder> {
         News news = this.news.get(position);
         holder.binding.tvTitle.setText(news.getTitle());
         holder.binding.tvDescription.setText(news.getDescription());
+        Picasso.get().load(news.getImage()).into(holder.binding.ivThumbnail);
+
     }
 
     @Override
