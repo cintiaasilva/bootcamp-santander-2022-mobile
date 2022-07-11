6841abe6cfe81a7488a6ab30f76f06a3ed75b4d7
cintiaasilva <cintiazl2015@gmail.com>
2022-07-11 12:33:07 -0300

    Intent Nativa para funcionalidade share

--- Soccer News/app/src/main/java/me/dio/soccernews/ui/adapter/NewsAdapter.java
+++ Soccer News/app/src/main/java/me/dio/soccernews/ui/adapter/NewsAdapter.java
@@ -17,7 +17,7 @@ import me.dio.soccernews.domain.News;
 
 public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.ViewHolder> {
 
-    private List<News> news;
+    private final List<News> news;
 
     public NewsAdapter(List<News> news) {
         this.news = news;
@@ -34,13 +34,29 @@ public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.ViewHolder> {
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         News news = this.news.get(position);
+
         holder.binding.tvTitle.setText(news.getTitle());
+
         holder.binding.tvDescription.setText(news.getDescription());
+
         Picasso.get().load(news.getImage()).into(holder.binding.ivThumbnail);
+
+        //Funcionalidade de abrir link
         holder.binding.btOpenLink.setOnClickListener(view ->{
             Intent i = new Intent(Intent.ACTION_VIEW);
             i.setData(Uri.parse(news.getLink()));
             holder.itemView.getContext().startActivity(i);
+        });
+
+        //Funcionalidade de compartilhar
+        holder.binding.ivShare.setOnClickListener(view ->{
+            Intent intent = new Intent(Intent.ACTION_SEND);
+            intent.setType("text/plain");
+            intent.putExtra(android.content.Intent.EXTRA_SUBJECT, news.getTitle()); //aparece o assunto
+            intent.putExtra(Intent.EXTRA_TEXT, news.getLink());
+            holder.itemView.getContext().startActivity(Intent.createChooser(intent, "Share"));
+            //createChooser = vai aparecer para o usuário escolher por qual meio ele quer
+            //compartilhar (whats, gmail,...)
         });
 
     }
